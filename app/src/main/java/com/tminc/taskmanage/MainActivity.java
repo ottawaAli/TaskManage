4a3cd81d57bc5c72ec44ceb8f240d933f7002960
ottawaAli <137909613+ottawaAli@users.noreply.github.com>
2023-12-05 19:35:59 -0500

    add two xml files and modify RegisterActivity.java to show password when click on the password_visibility_icon

--- app/src/main/java/com/tminc/taskmanage/MainActivity.java
+++ app/src/main/java/com/tminc/taskmanage/MainActivity.java
@@ -5,19 +5,22 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 
 
-public class MainActivity extends AppCompatActivity implements RegisterButtonFragment.OnRegisterButtonClickListener{
+public class MainActivity extends AppCompatActivity implements RegisterButtonFragment.OnRegisterButtonClickListener {
 
     private EditText user_name;
     private EditText password;
     private DatabaseHelper databaseHelper;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,4 +62,5 @@ public class MainActivity extends AppCompatActivity implements RegisterButtonFra
         Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
         startActivity(intent);
     }
+
 }
--- app/src/main/java/com/tminc/taskmanage/RegisterActivity.java
+++ app/src/main/java/com/tminc/taskmanage/RegisterActivity.java
@@ -5,9 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
@@ -15,16 +17,41 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText password;
     private EditText re_password;
     private DatabaseHelper databaseHelper;
+    private ImageView passwordVisibilityIcon; // 声明为类的成员变量
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        passwordVisibilityIcon = findViewById(R.id.password_visibility_icon);
+        user_name = findViewById(R.id.new_user_name);
+        password = findViewById(R.id.new_password);  // Initialize here
+        re_password = findViewById(R.id.reenter_password);
 
         // Initialize UI elements and the DatabaseHelper
         Button register_btn = findViewById(R.id.register_button);
         user_name = findViewById(R.id.new_user_name);
         password = findViewById(R.id.new_password);
         re_password = findViewById(R.id.reenter_password);
+
+        // 设置密码输入框的初始输入类型为密码
+        if (password != null) {
+            password.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+
+            // 添加显示/隐藏密码功能
+            passwordVisibilityIcon.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    togglePasswordVisibility();
+                }
+            });
+        } else {
+            // Handle the case where password EditText is not found
+            Toast.makeText(this, "Error: Password EditText not found", Toast.LENGTH_SHORT).show();
+        }
 
         databaseHelper = new DatabaseHelper(this);
 
@@ -54,4 +81,26 @@ public class RegisterActivity extends AppCompatActivity {
             }
         });
     }
+
+
+    // 切换密码可见性
+    private void togglePasswordVisibility() {
+        int inputType = password.getInputType();
+        if ((inputType & InputType.TYPE_TEXT_VARIATION_PASSWORD) != 0) {
+            // 密码模式，切换为可见文本
+            password.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+            passwordVisibilityIcon.setImageResource(R.drawable.ic_visibility_on);
+        } else {
+            // 可见文本模式，切换为密码
+            password.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+            passwordVisibilityIcon.setImageResource(R.drawable.ic_visibility_off);
+        }
+
+        // 重新获取焦点
+        password.requestFocus();
+
+        // 移动光标到末尾
+        password.setSelection(password.getText().length());
+    }
+
 }
